09fa608f03e7e3e8147ca2567e4dd02b25f04780
Said Aroua <said.aroua@bluewin.ch>
2026-03-27 17:00:39 +0100

    Refine the expected API data

--- frontend/src/Card.tsx
+++ frontend/src/Card.tsx
@@ -10,7 +10,7 @@ const Card: Component<EventProps> = (props) => {
     <a href={props.event.source_url} class='flex flex-col gap-6 border-1 p-3 shadow-md rounded-xl w-100 '>
       <div class='flex justify-between'>
         <h1 class='font-bold'>{props.event.event_title}</h1>
-        <h2 class='font-semibold'>org</h2>
+        <h2 class='font-semibold'>{props.event.source_name}</h2>
       </div>
       <p>{props.event.description || "empty"}</p>
       <div class='flex justify-between gap-4 mt-2'>
--- frontend/src/event.ts
+++ frontend/src/event.ts
@@ -1,12 +1,13 @@
 
 export type Event = {
   event_id: string,
+  source_name: string,
   source_url: string,
   event_title: string,
-  start_datetime: string,
+  start_date: string,
+  start_time: string,
   end_datetime?: string,
   location: string,
   description: string,
-  category?: string,
   extracted_at: string
 }
